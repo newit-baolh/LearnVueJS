becde6794c63595ecaa7ae2de5da5a8f8a752b43
baolh <baolh@newit.co.jp>
2022-02-11 10:17:12 +0700

    data and template

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const app = Vue.createApp({
     // data, function
-    template: '<h2>I\'m a template</h2>'
+    // template: '<h2>I\'m a template</h2>'
+    data() {
+        return {
+            title: 'The final Empire',
+            name: 'Book',
+            date: '11/2/2022'
+        }
+    },
 })
 
 app.mount('#app')
