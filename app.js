639e0acd82a306bce916862e35889075131a6987
baolh <baolh@newit.co.jp>
2022-02-11 13:58:42 +0700

    attribute binding

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const app = Vue.createApp({
     // template: '<h2>I\'m a template</h2>'
     data() {
         return {
+            url:'https://google.com.vn',
             showBooks: true,
             books : [
-                {title: 'Henry the sea', name: 'Lalala'},
-                {title: 'By the way', name: 'Orig'},
-                {title: 'Obanan yuton', name: 'Penata'},
+                {title: 'Henry the sea', name: 'Lalala', img: 'assets/1.jpg'},
+                {title: 'By the way', name: 'Orig', img: 'assets/2.jpg'},
+                {title: 'Obanan yuton', name: 'Penata', img: 'assets/3.jpg'},
             ]
         }
     },
