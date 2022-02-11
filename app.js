1442e3e4a30d14166fad8a886637f25b66ce9a24
baolh <baolh@newit.co.jp>
2022-02-11 12:08:07 +0700

    condition rendering

--- app.js
+++ app.js
@@ -3,16 +3,20 @@ const app = Vue.createApp({
     // template: '<h2>I\'m a template</h2>'
     data() {
         return {
+            showBooks: true,
             title: 'The final Empire',
             name: 'Book',
             date: '11/2/2022',
-            age: 10
+            age: 10,
         }
     },
     methods: {
         changeTitle(title){
             // this.title = ' The story of Mine'
             this.title = title
+        },
+        toggleBooks(){
+            this.showBooks = !this.showBooks
         }
     },
 })
