f881b9a8c77ef52b332695a33b231cdbe81ba3e9
newit_baolh <baolh@newit.co.jp>
2022-02-14 17:08:38 +0700

    folder structure

---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+      <h2>{{job.title}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+data(){
+    return {
+        jobs: [
+            {title: 'Ni',id: 1, details: 'poo'},
+            {title: 'Bi',id: 2, details: 'pee'},
+            {title: 'Ty',id: 3, details: 'nee'},
+        ]
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
