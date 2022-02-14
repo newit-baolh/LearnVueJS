d617464f2109bfe89491c95cfcb02b207026137c
newit_baolh <baolh@newit.co.jp>
2022-02-14 11:39:38 +0700

    new game

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Timer</h1>
+  <button class="btn-play" @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Block from './components/Block.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Block
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      console.log(this.delay);
+    }
+  },
 }
 </script>
 
@@ -20,7 +33,23 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+.btn-play{
+  padding: 10px;
+  border: none;
+  background: rgb(218, 238, 206);
+  
+}
+.btn-play:hover{
+  background: rgb(166, 241, 123);
+  border: 1px solid #333;
+  border-radius: 4px;
+  cursor: pointer;
+}
+.btn-play:disabled{
+  border: none;
+  background: rgb(218, 238, 206);
 }
 </style>
