1c672325b87b34256f9ce314eda18882c24dc0f5
newit_baolh <baolh@newit.co.jp>
2022-02-14 16:47:03 +0700

    router links

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, quae eius illo modi architecto tenetur dolor saepe amet voluptatum tempore ipsam! Atque voluptas, porro, quo magnam quis nulla praesentium nostrum sequi sapiente alias voluptates voluptatem nisi saepe optio nobis aliquam hic reiciendis minima, dolores doloribus. Ab voluptate laudantium ipsum nemo error aspernatur. Vel architecto doloremque illum exercitationem, est fuga quos eius laudantium et possimus facere magni nihil minus ipsam quia excepturi hic porro culpa placeat, corrupti consectetur. Vero autem earum deserunt atque quis porro corrupti optio quo placeat, excepturi, iusto, ratione velit deleniti quaerat amet minima numquam doloremque veniam! Ipsum?</p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id odio autem unde, cum optio corporis molestias quam debitis. Nam eum laboriosam hic veritatis nesciunt dolorem molestias magni, odit dolores vitae veniam provident? Eligendi veniam, nam incidunt adipisci, fugit corrupti perferendis eaque labore nisi accusantium quod laboriosam ab beatae corporis suscipit ipsam in! Nam quas iure quo, qui facilis alias eius? Magnam distinctio beatae accusantium neque rerum cum voluptates ipsa sunt asperiores voluptatum. Repellat inventore incidunt molestias enim vel modi hic, iure optio saepe exercitationem. Consectetur, incidunt. Voluptatem nostrum cupiditate doloribus dolorem consectetur nesciunt a aperiam ipsam consequatur? Veritatis, suscipit iste!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id odio autem unde, cum optio corporis molestias quam debitis. Nam eum laboriosam hic veritatis nesciunt dolorem molestias magni, odit dolores vitae veniam provident? Eligendi veniam, nam incidunt adipisci, fugit corrupti perferendis eaque labore nisi accusantium quod laboriosam ab beatae corporis suscipit ipsam in! Nam quas iure quo, qui facilis alias eius? Magnam distinctio beatae accusantium neque rerum cum voluptates ipsa sunt asperiores voluptatum. Repellat inventore incidunt molestias enim vel modi hic, iure optio saepe exercitationem. Consectetur, incidunt. Voluptatem nostrum cupiditate doloribus dolorem consectetur nesciunt a aperiam ipsam consequatur? Veritatis, suscipit iste!</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
