774de901367029efaa1be2d5bd85048fc029b8f5
newit_baolh <baolh@newit.co.jp>
2022-02-14 15:40:59 +0700

    delete skill

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -23,9 +23,9 @@
     </select>
 
     <label>Skill:</label>
-    <input type="text" v-model="tempSkill" @keyup="addSkill" />
+    <input type="text" v-model="tempSkill" @keyup.alt="addSkill" />
     <div v-for="skill in skills" :key="skill" class="pill">
-      {{ skill }}
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
     </div>
 
     <div class="term">
@@ -60,6 +60,11 @@ export default {
         this.tempSkill = ''
       }
     },
+    deleteSkill(skill){
+        this.skills = this.skills.filter((item)=>{
+            return skill !== item
+        })
+    }
   },
 }
 </script>
@@ -98,5 +103,17 @@ input[type='checkbox'] {
   margin: 0 10px 0 0;
   position: relative;
   top: 2px;
+}
+.pill{
+    display: inline-block;
+    margin: 20px 10px 0 0 ;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
 }
 </style>
