6d472ac49d8eacc81628c1e64e5bdbf57fcc75c8
baolh <baolh@newit.co.jp>
2022-02-16 00:23:17 +0700

    template ref

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="home">
     Home
-    <p>My name is {{name}} and age {{age}}</p>
+    <p ref="p">My name is {{name}} and age {{age}}</p>
     <button @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity';
 export default {
   name: 'Home',
   setup(){
     console.log('setupy');
 
+    const p =ref(null)
+
     let name = 'mario'
     let age = 30
     const handleClick = ()=>{
-      console.log('Click me');
+      console.log(p);
     }
 
     return {
-      name, age, handleClick
+      name, age, handleClick, p
     }
   },
   data(){
