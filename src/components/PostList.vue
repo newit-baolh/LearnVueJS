848ee3db3622b2b7bfb936407f836cce17a87eea
newit_baolh <baolh@newit.co.jp>
2022-02-16 09:01:43 +0700

    lifecycles hooks in setup

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -7,12 +7,15 @@
 </template>
 
 <script>
+import { onMounted, onUnmounted, onUpdated } from '@vue/runtime-core'
 import SinglePost from './SinglePost.vue'
 export default {
     props: ['posts'],
     components: {SinglePost},
     setup(props){
-        console.log(props);
+        onMounted(()=> console.log('component mounted'))
+        onUnmounted(()=> console.log('component unmounted'))
+        onUpdated(()=>  console.log('component updated'))
     }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList :posts = "posts"/>
+    <PostList v-if="showPosts" :posts = "posts"/>
+    <button @click="showPosts = !showPosts">toggle post</button>
+    <button @click="posts.pop()">delete post</button>
   </div>
 </template>
 
@@ -18,7 +20,11 @@ export default {
       {title: 'blog 1',body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At tempora eos voluptate maxime a laborum atque commodi debitis cum explicabo voluptas consectetur, quis aut ad neque illo sit pariatur dicta maiores. Porro obcaecati totam unde quidem nesciunt quisquam laboriosam, voluptates et, laudantium molestiae, fuga animi adipisci atque! Corporis voluptate dolore minus animi cumque recusandae, minima atque, rem illo non mollitia iusto fugit in consequuntur, soluta quasi eius at? Velit, cupiditate minima? Numquam, dolorem earum! Dignissimos neque cupiditate ducimus expedita tempora! Accusantium amet mollitia sit doloremque vel, libero architecto quas! Non praesentium accusantium optio quidem id ducimus. Saepe tempore ad molestiae.', id: 1},
       {title: 'blog 2',body: 'menu', id: 2}
     ])
-    return {posts}
+    const showPosts = ref(true)
+    return {posts, showPosts}
+  },
+  mounted(){
+    console.log('mounted using option api');
   }
 }
 </script>
