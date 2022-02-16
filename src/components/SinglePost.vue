1553960aa4201e1bb8ba78212b5477f0ddb349e5
newit_baolh <baolh@newit.co.jp>
2022-02-16 08:50:58 +0700

    using props in setup

---
+++ src/components/SinglePost.vue
@@ -0,0 +1,24 @@
+<template>
+ <div class="post">
+     <h3>{{post.title}}</h3>
+     <!-- <p>{{post.body}}</p> -->
+     <p>{{snipet}}</p>
+     </div> 
+</template>
+
+<script>
+import { computed } from '@vue/runtime-core'
+export default {
+    props: ['post'],
+    setup(props){
+        const snipet = computed(()=>{
+            return props.post.body.substring(0,100) + '...'
+        })
+        return {snipet}
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,24 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <input type="text" v-model="search">
-    <p>search term - {{search}}</p>
-    <div v-for="name in matchingNames" :key="name">{{name}}</div>
-    <button @click="handleClick">Stop watch</button>
+    <PostList :posts = "posts"/>
   </div>
 </template>
 
 <script>
-import { computed, ref, watch, watchEffect } from '@vue/runtime-core';
+import {ref} from '@vue/runtime-core';
+import PostList from '../components/PostList.vue'
 export default {
   name: 'Home',
+  components: {
+    PostList
+  },
   setup(){
-    const search = ref('')
-    const names = ref(['ni','bo','oi'])
-
-    const stopWatch = watch(search, ()=>{
-      console.log(' watch run');
-    })
-
-    const stopWatcheffect = watchEffect(()=>{
-      console.log('watcheffect run', search.value);
-    })
-
-    const matchingNames = computed(()=>{
-      return names.value.filter((name)=> name.includes(search.value))
-    })
-
-    const handleClick = ()=>{
-      stopWatch()
-      stopWatcheffect()
-    }
-    return {names, search, matchingNames, handleClick}
+    const posts = ref([
+      {title: 'blog 1',body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At tempora eos voluptate maxime a laborum atque commodi debitis cum explicabo voluptas consectetur, quis aut ad neque illo sit pariatur dicta maiores. Porro obcaecati totam unde quidem nesciunt quisquam laboriosam, voluptates et, laudantium molestiae, fuga animi adipisci atque! Corporis voluptate dolore minus animi cumque recusandae, minima atque, rem illo non mollitia iusto fugit in consequuntur, soluta quasi eius at? Velit, cupiditate minima? Numquam, dolorem earum! Dignissimos neque cupiditate ducimus expedita tempora! Accusantium amet mollitia sit doloremque vel, libero architecto quas! Non praesentium accusantium optio quidem id ducimus. Saepe tempore ad molestiae.', id: 1},
+      {title: 'blog 2',body: 'menu', id: 2}
+    ])
+    return {posts}
   }
 }
 </script>
