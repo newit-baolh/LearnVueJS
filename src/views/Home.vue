1050b8b096308fa2a73f3776497331d48458930e
newit_baolh <baolh@newit.co.jp>
2022-02-16 09:34:48 +0700

    fetching data in setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList v-if="showPosts" :posts = "posts"/>
-    <button @click="showPosts = !showPosts">toggle post</button>
-    <button @click="posts.pop()">delete post</button>
+    <div v-if="error">{{error}}</div>
+    <div v-if="posts.length">
+      <PostList :posts = "posts"/>
+    </div>
+    <div v-else>Loading ...</div>
   </div>
 </template>
 
@@ -16,12 +18,23 @@ export default {
     PostList
   },
   setup(){
-    const posts = ref([
-      {title: 'blog 1',body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At tempora eos voluptate maxime a laborum atque commodi debitis cum explicabo voluptas consectetur, quis aut ad neque illo sit pariatur dicta maiores. Porro obcaecati totam unde quidem nesciunt quisquam laboriosam, voluptates et, laudantium molestiae, fuga animi adipisci atque! Corporis voluptate dolore minus animi cumque recusandae, minima atque, rem illo non mollitia iusto fugit in consequuntur, soluta quasi eius at? Velit, cupiditate minima? Numquam, dolorem earum! Dignissimos neque cupiditate ducimus expedita tempora! Accusantium amet mollitia sit doloremque vel, libero architecto quas! Non praesentium accusantium optio quidem id ducimus. Saepe tempore ad molestiae.', id: 1},
-      {title: 'blog 2',body: 'menu', id: 2}
-    ])
-    const showPosts = ref(true)
-    return {posts, showPosts}
+    const posts = ref([])
+    const error = ref(null)
+
+    const load = async ()=>{
+      try {
+        let data = await fetch("http://localhost:3000/posts")
+        if(!data.ok){
+          throw Error('no data availabe')
+        }
+        posts.value = await data.json()
+      } catch (err) {
+        error.value = err.message
+        console.log(error.value);
+      }
+    }
+    load()
+    return {posts, error}
   },
   mounted(){
     console.log('mounted using option api');
